a9cd86edc856dbde6ce8c8c63a1c97aa4e5c261b
Sabshor <lsabshor@mail.ru>
2024-03-01 13:02:29 +0300

    add plugin application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,23 @@
 plugins {
+    application
     id("java")
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
+application {
+    mainClass = "hexlet.code.App"
+}
+
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
 }
 
 tasks.test {
