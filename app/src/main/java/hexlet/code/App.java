d5d41840cd23e6c4a7144e99880d98ae6c88173d
Sabshor <lsabshor@mail.ru>
2024-03-04 16:57:57 +0300

    amend added game Greet

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,27 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.println("Your choice: ");
+        Scanner scanner = new Scanner(System.in);
+        int indexGame = scanner.nextInt();
+        switch (indexGame) {
+            case 0:
+                //System.exit(0);
+                break;
+            case 1:
+                System.out.println("Welcome to the Brain Games!");
+                Cli.greeting(scanner);
+                break;
+            default:
+                System.out.println("Введено некорректное число");
+        }
+        scanner.close();
     }
 }
