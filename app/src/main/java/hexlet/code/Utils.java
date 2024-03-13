4d2f0fa80ab4ed9e2e523f819e1c2b991e702975
Sabshor <lsabshor@mail.ru>
2024-03-13 11:58:36 +0300

    add Utils.java

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -1,2 +1,20 @@
-package hexlet.code;public class Utils {
+package hexlet.code;
+
+public class Utils {
+    public static int getRandomNumber() {
+        final int rank = 100;
+        int number = (int) (Math.random() * rank);
+        while (number == 0) {
+            number = (int) (Math.random() * rank);
+        }
+        return number;
+    }
+    public static int getRandomDigit(int minValue, int maxValue) {
+        final int rank = 10;
+        int number = (int) (Math.random() * rank);
+        while ((number < minValue) || (number > maxValue)) {
+            number = (int) (Math.random() * rank);
+        }
+        return number;
+    }
 }
